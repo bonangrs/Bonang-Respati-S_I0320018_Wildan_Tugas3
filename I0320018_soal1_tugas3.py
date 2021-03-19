7d3386561c6f39dd429dab06e6bedfb4f2c6d43e
Bonang <satbonang@gmail.com>
2021-03-20 01:34:20 +0700

    push soal 1 & 2

--- I0320018_soal1_tugas3.py
+++ I0320018_soal1_tugas3.py
@@ -1,25 +1,23 @@
-listTeman = ['Toni', 'Maurich','Janet', 'Yola', 'Yuku', 'Vizal', 'Densur', 'Fajri', 'Sekar', 'Zapira']
+listTeman = ['Safri', 'Zaneta', 'Yola', 'Maurich', 'Yuku', 'Vizal', 'Densur', 'Fajri', 'Sekar', 'Zapira']
 
 print('Nama teman saya yang berada di indeks 4 adalah', listTeman[4])
 print('Nama teman saya yang berada di indeks 6 adalah', listTeman[6])
 print('Nama teman saya yang berada di indeks 7 adalah', listTeman[7])
 
-listTeman[3] = "Lutpi"  # mengganti nama teman
+listTeman[3] = "Toni"  # mengganti nama teman di indeks 3, 5, dan 9
 listTeman[5] = "Dika"
 listTeman[9] = "Aril"
 
-listTeman.append("Eca") # menambah nama teman
-listTeman.append("Nabil")
+listTeman.extend(["Eca", "Nabil"])
 
 print("Teman-teman saya bernama", end=" ")  # menampilkan nama-nama teman
 for namaTeman in listTeman:
     if namaTeman == listTeman[-1]:
-        print(namaTeman, end='.')
+        print(namaTeman, end='. \n')
         break
     if namaTeman == listTeman[-2]:
         print(namaTeman, end=', dan ')
     else:
         print(namaTeman, end=', ')
 
-print(" ")
-print("Jumlah teman dalam list: ", len(listTeman))
+print("Jumlah nama teman dalam list: ", len(listTeman))
--- I0320018_soal2_tugas3.py
+++ I0320018_soal2_tugas3.py
@@ -1,49 +1,20 @@
-# masukin data awal sesuai soal
+# membuat dict
 dictAwal = {'Nama': 'Bonang Respati S.',
-        'Hobi 1': 'Membaca',
-        'Hobi 2': 'Tidur',
-        'Hobi 3': 'Bermain game',
-        'Media sosial 1': '@bonangrs (IG)',
-        'Media sosial 2': '@bonangrs (Twitter)',
-        'Media sosial 3': 'LINE satbonang',
-        'Lagu favorit 1': 'Sayang - Via Vallen',
-        'Lagu favorit 2': 'Perlahan - GuyonWaton',
-        'Lagu favorit 3': 'Rahasia - Payung Teduh',
-        'Makanan favorit 1': 'Sate kambing',
-        'Makanan favorit 2': 'Mie yamin',
-        'Makanan favorit 3': 'Nasi goreng'}
+        'Hobi': ['baca buku', 'tidur', 'main game'],
+        'Twitter': '@bonangrs',
+        'IG': '@bonangrs',
+        'LINE': 'satbonang',
+        'Lagu favorit': ['Sayang - Via Vallen',
+                         'Perlahan - GuyonWaton',
+                         'Rahasia - Payung Teduh'],
+        'Makanan favorit': ['nasi goreng', 'mie yamin', 'sate kambing'] }
 
-dictAwal['Hobi 2'] = 'Menonton YouTube'     # ubah hobi
-dictAwal['Media sosial 3'] = 'Bonang Respati (FB)'      # ubah medsos
-dictAwal.pop('Makanan favorit 2')       # hapus makanan favorit
-del dictAwal['Makanan favorit 3']       # hapus makanan favorit
-dictAwal.update({'Hobi 4': 'Bermaain Lego'})      # tambah hobi
+dictAwal['Hobi'][1] = 'menonton youtube'            # mengubah value hobi
+dictAwal['FB'] = dictAwal.pop('LINE')               # mengganti key media sosial
+dictAwal['FB'] = 'Bonang Respati'                   # menambahkan value media sosial
+dictAwal['Makanan favorit'].remove('nasi goreng')   # menghapus value
+dictAwal['Makanan favorit'].remove('mie yamin')
+dictAwal['Hobi'].append('belajar')                  # menambahkan hobi
 
-listAwal = []                # inisiasi awal
-for k,v in dictAwal.items():     # k utk key, v utk value
-    listAwal.append((str(k) + ": " + str(v)))
-
-listNama = []           #inisiasi awal
-listHobi = []
-listMedsos = []
-listMakanan = []
-listLagu = []
-
-for x in listAwal:  # ngebagi list awal yg msh acak ke list per bagian(nama, hobi, medsos, dll)
-    if 'Hobi' in x:
-        listHobi.append(x)
-        # listHobi.sort()   # bisa di-sort buat urutin
-    if 'Nama' in x:
-        listNama.append(x)
-    if 'Media' in x:
-        listMedsos.append(x)
-    if 'Lagu' in x:
-        listLagu.append(x)
-    if 'Makanan' in x:  # klo pake else hasilnya ngulang lagi dari awal, jdnya pake if
-        listMakanan.append(x)
-
-listFinal = listNama + listHobi + listMedsos + listLagu + listMakanan   # gabungin list per bagian mjd 1 list
-
-for kelar in listFinal:
-    print(kelar)
+print(dictAwal)
 
